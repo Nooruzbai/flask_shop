d81cebc5f70aee82e8c6b67c34f647659a2bf55e
nooruzbai <nooruzbay@gmail.com>
2023-07-01 14:11:20 +0600

    Added user models

---
+++ source/auth_app/user_models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from flask_login import UserMixin
+from sqlalchemy import func
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from source.extensions import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    phone_number = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    role = db.Column(db.String)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
