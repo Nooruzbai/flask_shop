6b7ef133393a97e4e1e6e2090725a6e0f8afd05f
nooruzbai <nooruzbay@gmail.com>
2023-07-01 14:11:35 +0600

    Added user forms

---
+++ source/auth_app/auth_forms.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import (StringField, RadioField, EmailField, validators,
+                     PasswordField, DateTimeField, SubmitField)
+from wtforms.validators import Length, EqualTo, DataRequired, Email
+
+
+class RegistrationForm(FlaskForm):
+    email = EmailField('Email', [DataRequired(), validators.Email()])
+    password = PasswordField(validators=[validators.Length(min=2, message='Too short')])
+    password_confirm = PasswordField(validators=[EqualTo('password', 'Password mismatch')])
+    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=5, max=30)])
+    created_at = DateTimeField("Created_at", format="%Y-%m-%dT%H:%M:%S",
+                               default=datetime.today,
+                               validators=[DataRequired()])
+    submit = SubmitField(label='Submit')
+    role = RadioField('Role', choices=['Admin', 'User'], default='User')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    # remember = BooleanField('Remember Me', validators=[DataRequired()])
+    submit = SubmitField('Login')
